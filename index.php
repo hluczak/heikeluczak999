af96caa08f1959ddfa1f358b968831ad69f6ab00
hluczak <luczak@me.com>
2013-11-03 12:03:02 +0100

    1 Hinzufuegung zu bootstrap datei zu dem Projekt

---
+++ index.php
@@ -0,0 +1,9 @@
+<html>
+<head>
+<title>Heike luczak / Cloud Control :: Test Project</title>
+</head>
+<body>
+<h1>Heike Luczak/ Cloud Control :: Test Project</h1>
+<p>Irgendein Geschreibsel</p>
+</body>
+</html>
