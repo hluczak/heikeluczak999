f60b5b424976b378f502bc4b539a1c801d49165a
hluczak <luczak@me.com>
2013-11-04 08:38:47 +0100

    2korrigierter commit 4.11

--- index.php
+++ index.php
@@ -3,8 +3,9 @@
 <title>Heike luczak / Cloud Control :: Test Project</title>
 </head>
 <body>
+<header>Kopfbereich</header>
 <h1>Heike Luczak/ Cloud Control :: Test Project</h1>
 <p>Irgendein Geschreibsel</p>
-<p>Neue Texthinzufügung</p>
+<p>Neue TexthinzufÃ¼gung</p>
 </body>
 </html>
